f646a54680f8bc61b9e1eb96c291d66ee3908e89
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-07 10:21:24 -0400

    feat (src/views): added text content in HomeViewContent component

---
+++ src/views/HomeContent.jsx
@@ -0,0 +1,88 @@
+import Collapse from "ui/Collapse"
+
+export default function HomeViewContent() {
+    return (
+        <div>
+            <Collapse title="Propósito del Proyecto">
+                El propósito fundamental de "SaborVital" es fomentar el bienestar y la salud a través de dos pilares esenciales: la alimentación y los hábitos. Queremos empoderarte con el conocimiento y las herramientas necesarias para tomar decisiones conscientes en tus desayunos y, al mismo tiempo, crear hábitos que perduren en el tiempo.
+                Bienvenido a "SaborVital ", donde los sabores saludables y los hábitos brillantes se unen para crear un camino hacia tu mejor versión. ¡Prepárate para un viaje emocionante hacia el bienestar!
+            </Collapse>
+
+            <Collapse title="Beneficios de Desayunos Saludables">
+                Los desayunos saludables son esenciales para el bienestar. Ofrecen una serie de ventajas significativas:
+
+                <ol>
+                    <li><h4>Energía y Vitalidad:</h4> Comenzar el día con nutrientes adecuados proporciona energía y vitalidad para afrontar las tareas diarias.</li>
+                    <li><h4>Mejora Cognitiva:</h4> Estos desayunos potencian la concentración y la función mental.</li>
+                    <li><h4>Control de Peso:</h4> Ayudan a mantener un peso saludable al controlar el apetito durante el día.</li>
+                    <li><h4>Metabolismo Activo:</h4> Estimulan el metabolismo, favoreciendo la quema de calorías.</li>
+                    <li><h4>Estabilidad en Azúcar en Sangre:</h4> Un desayuno equilibrado regula los niveles de azúcar en sangre.</li>
+                    <li><h4>Nutrientes Esenciales:</h4> Proporcionan vitaminas y minerales esenciales para una salud óptima.</li>
+                    <li><h4>Salud Cardiovascular:</h4> Alimentos como aguacate y nueces mejoran la salud del corazón.</li>
+                    <li><h4>Hidratación:</h4> Incluir líquidos saludables mantiene la hidratación.</li>
+                    <li><h4>Digestión Saludable:</h4> La fibra en el desayuno favorece una digestión sana.</li>
+                    <li><h4>Fomenta Buenos Hábitos:</h4> Establecer el hábito de un desayuno saludable puede conducir a elecciones alimentarias más equilibradas.</li>
+                </ol>
+            </Collapse>
+
+            <Collapse title="Consejos para un Desayuno Saludable">
+                Para un desayuno saludable y delicioso, ten en cuenta estos consejos:
+
+                <ol>
+                    <li><h4>Variedad Nutricional:</h4> Incluye alimentos de distintos grupos: proteínas, granos enteros, frutas y grasas saludables. Esto asegura una gama completa de nutrientes.</li>
+                    <li><h4>Proteína:</h4> Agrega proteínas magras como huevos, yogur o queso cottage. Ayudan a mantener la saciedad y los niveles de energía.</li>
+                    <li><h4>Carbohidratos Complejos:</h4> Opta por granos enteros como avena, pan integral o quinua. Son ricos en fibra y liberan energía de manera sostenida.</li>
+                    <li><h4>Frutas Frescas:</h4> Las frutas aportan vitaminas, minerales y antioxidantes. Puedes incluirlas en rodajas o batidos.</li>
+                    <li><h4>Grasas Saludables:</h4> Fuentes como aguacate, nueces o aceite de oliva aportan grasas esenciales para el cerebro y la piel.</li>
+                    <li><h4>Fibra:</h4> La fibra ayuda en la digestión y la regulación del azúcar en sangre. Añade cereales integrales o semillas de chía.</li>
+                    <li><h4>Evita Azúcares Agregados:</h4> Minimiza alimentos procesados ricos en azúcares. Opta por endulzar con miel o frutas.</li>
+                    <li><h4>Hidratación:</h4> Bebe agua, té o jugo natural junto al desayuno. La hidratación es clave para el funcionamiento óptimo del cuerpo.</li>
+                    <li><h4>Porciones Moderadas:</h4> Controla las porciones para evitar el exceso de calorías.</li>
+                    <li><h4>Planificación:</h4> Dedica tiempo a preparar un desayuno equilibrado. Planificar con antelación ayuda a evitar opciones poco saludables en momentos de prisa.</li>
+                </ol>
+                Recuerda que un desayuno saludable es una inversión en tu bienestar. Adaptar estas pautas a tus preferencias personales te permitirá disfrutar de comidas matutinas deliciosas y nutritivas.
+            </Collapse>
+
+            <Collapse title="Cómo Mantener un Hábito Saludable">
+                Mantener un hábito saludable puede ser un desafío, pero con determinación y estrategias adecuadas, es totalmente posible. Aquí hay algunos pasos que puedes seguir para ayudar a mantener tus hábitos saludables:
+
+                <ol>
+                    <li><h4>Establece Objetivos Claros:</h4> Define claramente lo que quieres lograr con tu hábito. Tener un objetivo en mente te dará una razón sólida para mantenerlo.</li>
+                    <li><h4>Comienza Pequeño:</h4> Si estás tratando de incorporar un hábito completamente nuevo, comienza con pequeños pasos. Esto reduce la resistencia al cambio y te permite construir gradualmente el hábito.</li>
+                    <li><h4>Sé Consistente:</h4> La consistencia es clave para mantener un hábito. Trata de realizar la misma acción en el mismo momento del día o en la misma situación. Esto ayuda a incorporar el hábito en tu rutina diaria.</li>
+                    <li><h4>Registra tu Progreso:</h4> Lleva un registro de tu progreso. Esto puede ser en forma de un diario, una aplicación o cualquier otro método que funcione para ti. Ver cómo avanzas puede ser muy motivador.</li>
+                    <li><h4>Encuentra un Recordatorio:</h4> Asocia tu hábito con algo que ya haces regularmente. Por ejemplo, si quieres hacer ejercicio todas las mañanas, hazlo justo después de cepillarte los dientes. El hábito se convierte en una extensión natural de una actividad existente.</li>
+                    <li><h4>Mantén la Motivación:</h4> Encuentra formas de mantenerte motivado. Esto puede incluir recompensarte a ti mismo por alcanzar hitos o recordarte por qué te embarcaste en este hábito en primer lugar.</li>
+                    <li><h4>Supera las Barreras:</h4> Identifica las barreras que puedan surgir y encuentra estrategias para superarlas. Si no tienes tiempo para cocinar desayunos saludables todas las mañanas, planifica y prepara con anticipación.</li>
+                    <li><h4>Busca Apoyo:</h4> Comparte tus objetivos con amigos o familiares que puedan apoyarte. Pueden animarte y ayudarte a mantenerte responsable.</li>
+                    <li><h4>Sé Amable Contigo Mismo:</h4> Si tienes un tropiezo ocasional, no te culpes. Todos cometemos errores. Lo importante es volver al hábito sin desanimarte.</li>
+                    <li><h4>Celebra tus Logros:</h4> Reconoce y celebra tus logros. Ya sea un mes de mantener tu hábito o haber alcanzado un objetivo específico, tómate un momento para celebrar tu éxito.</li>
+                </ol>
+
+                Recuerda que mantener un hábito saludable lleva tiempo y esfuerzo, pero los beneficios para tu salud y bienestar valen la pena. Con paciencia y perseverancia, puedes convertir estos hábitos en una parte integral de tu vida diaria.
+
+            </Collapse>
+
+            <Collapse title="DESAFÍO : PLAN SEMANAL">
+
+                <ul>
+                    <li><a href="https://drive.google.com/file/d/1sMBp1Oa-R22bCgygEnOjAWy2x7wOIoWP/view?usp=drivesdk">Día 1</a></li>
+                    <li><a href="https://drive.google.com/file/d/185KDpiBXzGBRtyOQZt7jqr-DbHcFdGDq/view?usp=drivesdk">Día 2</a></li>
+                    <li><a href="https://drive.google.com/file/d/10dy-fleIaaatcco5PFZO6q3Zzx-3_K_Z/view?usp=drivesdk">Día 3</a></li>
+                    <li><a href="https://drive.google.com/file/d/1zD58KDGWBZ3IjRaF_VTZHE7li9dFVV6m/view?usp=drivesdk">Día 4</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ofwdqCnRbkzHHlzaiLgKFWnEcqyzhusQ/view?usp=drivesdk">Día 5</a></li>
+                    <li><a href="https://drive.google.com/file/d/1Ijg_wUIW-OgBfdaygOaImfEuaVfvF1S6/view?usp=drivesdk">Día 6</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ZJlt19VI-i8oBGtB0nI5zlB8n0Sbrds5/view?usp=drivesdk">Día 7</a></li>
+                </ul>
+
+                "¡Gracias por unirte a SaborVital: Desayunos que cuidan, hábitos que elevan! Esperamos que encuentres inspiración y consejos útiles para transformar tus mañanas en momentos de energía y bienestar.
+
+                Nuestra comunidad está creciendo, y tú puedes ser parte de esta revolución hacia una vida más saludable. Comparte tus propias recetas, fotos de tus desayunos y tus mejores consejos para mantener hábitos saludables. Juntos, podemos crear un espacio donde todos puedan disfrutar de desayunos deliciosos y nutritivos.
+
+                Y recuerda, cada pequeño paso hacia un desayuno más saludable es un gran avance en tu camino hacia un estilo de vida mejor.
+
+                Únete a nosotros y descubre el poder de comenzar tu día con SaborVital. ¡Tu cuerpo y mente te lo agradecerán!"
+            </Collapse>
+        </div>
+    )
+}
