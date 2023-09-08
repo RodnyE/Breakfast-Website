fd4ce7eaff04e6e5ead7c50430ce659bfa2703bb
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-09-08 12:50:19 -0400

    fix (serv): added fake views and fixed view EP to post

--- serv/router/router.js
+++ serv/router/router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const views = require("./views.js");
 const {post , getPost} = require("./post.js");
 
-router.get("/views" , views);
+router.post("/views" , views);
 router.post("/getpost" , getPost);
 router.post("/post" , post);
 
--- serv/router/views.js
+++ serv/router/views.js
@@ -3,20 +3,33 @@ const {User} = require(config.SERV + "/helpers/db.js");
 const uid = require(config.SERV + "/helpers/uid.js");
 
 const views = async (req, res) => {
-    const ip = req.socket.remoteAddress;
+    if (!req.body) return res.json({ status: false, data: { message: "NO_DATA" } });
+    let fp;
+    try {
+        const body = req.body;
+        fp = body.fp;
+    } catch (err) {
+        return res.json({
+            status: false,
+            data: {
+                message: "DATA_ERROR",
+                error: err
+            }
+        });
+    }
     const already = await User.findOne({
         where: {
-            user_ip: ip
+            user_ip: fp
         }
     });
     if (!already) {
         const user = await User.create({
-            user_ip: ip,
+            user_ip: fp,
             name: "user_" + uid.num(4)
         });
     }
 
-    const v = (await User.findAll()).length;
+    const v = (await User.findAll()).length + 2979;
 
     return res.json({views: v})
     
